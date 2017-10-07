9898108ebac8f21695222f5f9cdc05a65245e93e
KwintenP <I6+c36gtu3>
2017-10-07 17:49:33 +0200

    Added the rest repo.

---
+++ src/main/java/com/kwintenp/swapibackend/repositories/StarWarsCharacterRepository.java
@@ -0,0 +1,9 @@
+package com.kwintenp.swapibackend.repositories;
+
+import com.kwintenp.swapibackend.entities.StarWarsCharacter;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "starwarscharacters", path = "starwarscharacters")
+public interface StarWarsCharacterRepository extends MongoRepository<StarWarsCharacter, String> {
+}
