0283c1f1bc5271e7bc9b62986691b40d1ef223ec
Kwinten Pisman <kwinten.pisman@gmail.com>
2017-10-07 16:56:12 +0200

    initial spring boot project

---
+++ src/main/java/com/kwintenp/swapibackend/SwapiBackendApplication.java
@@ -0,0 +1,12 @@
+package com.kwintenp.swapibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwapiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwapiBackendApplication.class, args);
+	}
+}
