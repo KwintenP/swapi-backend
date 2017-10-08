8aa4f83f0af7c3b7730c47fb5d85c9a3522da2fd
KwintenP <I6+c36gtu3>
2017-10-08 22:24:13 +0200

    Tried to add bean interceptor and exposed the id's

--- src/main/java/com/kwintenp/swapibackend/repositories/StarWarsCharacterRepository.java
+++ src/main/java/com/kwintenp/swapibackend/repositories/StarWarsCharacterRepository.java
@@ -7,3 +7,9 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource(collectionResourceRel = "starwarscharacters", path = "starwarscharacters")
 public interface StarWarsCharacterRepository extends MongoRepository<StarWarsCharacter, String> {
 }
+
+
+//    @Bean
+//    public MappedInterceptor myMappedInterceptor() {
+//        return new MappedInterceptor(new String[]{"/**"}, new MyInterceptor());
+//    }
